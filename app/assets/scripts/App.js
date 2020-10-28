0fbe010a0a7e431b047533b76b810ad3551c393e
Yoshi <st27ave3@gmail.com>
2020-10-28 16:08:12 +0900

    Completed lazy loading of images.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,4 +1,5 @@
 import '../styles/styles.css'
+import 'lazysizes'
 import MobleMenu from './modules/MobileMenu'
 import RevealOnScroll from './modules/RevealOnScroll'
 import StickyHeader from './modules/StickyHeader'
